9aa69f2a4a76372968eb9c53166a933758975690
Zach Cornute <zachcornute@gmail.com>
2021-12-07 21:00:54 -0600

    adding solution to day-1 part 2

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
-
+# ----------------------------------- PART 1 ----------------------------------
 # read the input data for day-1
 with open("day1-input.txt", "r") as day1_input:
     # convert the input into integers
@@ -15,5 +15,25 @@ for reading in readings[1:]:
 
     # set previous reading to the current reading
     previous_reading = reading
+
+print(num_of_increases)
+
+
+# ----------------------------------- PART 2 ----------------------------------
+num_of_increases = 0
+previous_sum = sum(readings[0:3])  # store the first sum (first three readings)
+# again, starting from the second reading
+for i in range(1, len(readings) - 1):
+    # if we are unable to get a sum of three readings, exit loop
+    if (i + 3) > len(readings):
+        break
+    
+    # get the current sum and compare to the previous
+    current_sum = sum(readings[i:i+3])
+    if previous_sum < current_sum:
+        num_of_increases += 1
+    
+    # set previous sum to the current sum
+    previous_sum = current_sum
 
 print(num_of_increases)
